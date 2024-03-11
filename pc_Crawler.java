b707ea52ac94327a5524e3a4ec81293a2f9755f0
jorgegg02 <jorgegg02.jgg@gmail.com>
2024-03-11 23:17:53 +0100

    atributos estaticos pc_crawler

--- pc_Crawler.java
+++ pc_Crawler.java
@@ -9,12 +9,10 @@ public class pc_Crawler {
     
     private static TreeMap<String, List<String>> thesaurus = new TreeMap<>();
 
-    public static TreeMap crawling (String puntoEntrada, String separadores) {
+    public static void crawling (String puntoEntrada, String separadores) {
         
-        //TODO revisar parametrización de crawling
-    
         Queue<File> queue = new LinkedList<>();
-        Map <String, Integer> map = new TreeMap <String, Integer> ();
+        
         File fichero = new File(puntoEntrada);
     
         queue.add(new File(puntoEntrada));
@@ -42,19 +40,20 @@ public class pc_Crawler {
                     while (st.hasMoreTokens () ) {
                        
                         String s = st.nextToken().toLowerCase();
-
-                        // s = s.replaceAll("á", "a").replaceAll("é", "e").replaceAll("í", "i").replaceAll("ó", "o").replaceAll("ú", "u");
+                        System.out.println(s);
+                        s = s.replaceAll("á", "a").replaceAll("é", "e").replaceAll("í", "i").replaceAll("ó", "o").replaceAll("ú", "u");
+                        System.out.println(s);
                         s = Normalizer.normalize(s, Normalizer.Form.NFD);
                         System.out.println(s);
                         Object o = thesaurus.get(s);
                         System.out.println(o);
 
                         if (o != null){
-                            o = map.get(s);
-                            if (o == null) map.put (s, new Integer (1));
+                            o = dic.get(s);
+                            if (o == null) dic.put (s, new Integer (1));
                             else {
                                 Integer cont = (Integer) o;
-                                map.put (s, new Integer (cont.intValue () + 1));
+                                dic.put (s, new Integer (cont.intValue () + 1));
                             }
                         }
                     }
@@ -69,68 +68,47 @@ public class pc_Crawler {
         //     System.out.println(entry.getKey() + " : " + entry.getValue());
         // } 
 
-        return (TreeMap) map;
     }
 
-    public static TreeMap<String,  List<String>> crearThesauro (String puntoEntrada, String separadores) {
+    public static void  crearThesauro () {
         
-        //TODO revisar parametrización de crawling
+        File fichero = new File("theasaurus.txt");
     
-        Queue<File> queue = new LinkedList<>();
-        TreeMap <String, List<String>> map = new TreeMap <String,  List<String>> ();
-        File fichero = new File(puntoEntrada);
-    
-        queue.add(new File(puntoEntrada));
-    
-        while (!queue.isEmpty()) {
-            fichero = queue.poll();
             System.out.println(fichero.getName());
-    
-            if (fichero.isDirectory()) {
-                if (!fichero.exists() || !fichero.canRead()) {
-                    System.out.println("ERROR. No puedo leer " + fichero);
-                    continue;
-                }
-                File[] listaFicheros = fichero.listFiles();
-                if (listaFicheros != null) {
-                    queue.addAll(Arrays.asList(listaFicheros));
-                }
-            }
-            else try {
+   
+            try {
                 FileReader fr = new FileReader(fichero);
                 BufferedReader br = new BufferedReader(fr);
                 String linea;
                 while ((linea=br.readLine()) != null) {
-                    StringTokenizer st = new StringTokenizer(linea, separadores);
+
+                    StringTokenizer st = new StringTokenizer(linea, ",.:;(){}!°?\t''%/|[]<=>&#+*$-¨^~");
                     
                     List<String> sinonimos = new ArrayList<String>();
                     while (st.hasMoreTokens () ) {
                        
                         String s = st.nextToken().toLowerCase();
-                        // s = s.replaceAll("á", "a").replaceAll("é", "e").replaceAll("í", "i").replaceAll("ó", "o").replaceAll("ú", "u");
                         s = Normalizer.normalize(s, Normalizer.Form.NFD);
 
                         sinonimos.add(s);
                     }
 
                     for (String s : sinonimos) {
-                        map.put(s, sinonimos);
+                        thesaurus.put(s, sinonimos);
                     }
                     
                 }
                 br.close();
             }
             catch (Exception fnfe) {
-                System.out.println("ERROR. Fichero desaparecido en combate  ;-)");
+                System.out.println("ERROR. Thesauro.txt desaparecido en combate  ;-)");
             }
-        } 
+    } 
         // for (Map.Entry<String, Integer> entry : map.entrySet()) {
         //     System.out.println(entry.getKey() + " : " + entry.getValue());
         // } 
 
         // System.out.println(map);
-        return  map;
-    }
 
     
     public static void salvarObjeto (String fichero, Object o) {
@@ -164,14 +142,13 @@ public class pc_Crawler {
             return;
         }
 
-    
         
         // Comprobamos que existe un fichero llamado theasaurus.ser y si existe, cargamos el theasaurus usando readObject("theasaurus.ser", TreeMap(theasaurus)
         // Si no existe, creamos un theasaurus y lo guardamos en el fichero theasaurus.ser
         File f2 = new File("thesaurus.ser");
         if (!f2.exists()) {
             System.out.println("No existe el fichero theasaurus.ser");
-            thesaurus = crearThesauro("theasaurus.txt", ",.:;(){}!°?\t''%/|[]<=>&#+*$-¨^~");
+            crearThesauro();
             salvarObjeto("thesaurus.ser", thesaurus);
         }
 
@@ -182,7 +159,7 @@ public class pc_Crawler {
         File f = new File("diccionario.ser");
         if ( !f.exists()) {
             System.out.println("No existe el fichero diccionario.ser");
-            dic = crawling(args[0], " ,.:;(){}!°?\t''%/|[]<=>&#+*$-¨^~");
+            crawling(args[0], " ,.:;(){}!°?\t''%/|[]<=>&#+*$-¨^~");
             salvarObjeto("diccionario.ser", dic);
         }
         
